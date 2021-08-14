57293605c8ac2bc14d9b73a93ef92092e1533723
Jason Harrer <jazzyeagle79@gmail.com>
2021-08-14 13:42:31 -0500

    Form added and logic implemented to add new user

--- users_app/urls.py
+++ users_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('new_user_form', views.new_user_form)
 ]
--- users_app/views.py
+++ users_app/views.py
@@ -5,3 +5,13 @@ from .models import *
 def index(request):
     context = { 'users': User.objects.all() }
     return render(request, 'index.html', context)
+
+
+def new_user_form(request):
+    new_user = User.objects.create(
+                        first_name    = request.POST['first_name'],
+                        last_name     = request.POST['last_name'],
+                        email_address = request.POST['email_address'],
+                        age           = request.POST['age']
+                       )
+    return redirect('/')
